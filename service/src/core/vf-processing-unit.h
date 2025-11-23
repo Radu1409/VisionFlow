9f9a390c05965c766ac892b2d0e37250b7c39150
Radu Purecel <radu.purecel20@gmail.com>
2025-11-23 19:48:06 +0200

    service: core: Add processing unit types
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

--- service/src/core/vf-processing-unit.h
+++ service/src/core/vf-processing-unit.h
@@ -17,5 +17,35 @@
 #ifndef VF_PROCESSING_UNIT__H
 #define VF_PROCESSING_UNIT__H
 
+#include <sys/types.h>
+
+#include "vf-error.h"
+
+typedef vf_err_t (*operation_fp_t)(void *, ...);
+
+typedef struct operations {
+        operation_fp_t init;
+        operation_fp_t deinit;
+        operation_fp_t get_data;
+        operation_fp_t send_data;
+        operation_fp_t process_data;
+} operations_st_t;
+
+typedef enum unit_type {
+        UNIT_TYPE_INVALID    = -1,
+        UNIT_TYPE_CAMERA     = 0,
+        UNIT_TYPE_DISPLAY    = 1,
+        UNIT_TYPE_ENCODER    = 2,
+        UNIT_TYPE_CONVERSION = 3,
+} unit_type_t;
+
+typedef struct unit {
+        unit_type_t       type;
+        const char        *name;
+        struct operations operations;
+        union {
+        } settings;
+} unit_st_t;
+
 #endif /* VF_PROCESSING_UNIT__H */
 
