15c08c2a091ac2036c1ab23d6f70fa52ee6190c0
Radu Purecel <radu.purecel20@gmail.com>
2025-11-24 13:29:34 +0200

    libs: common: Update error header
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

--- libraries/vf-common/public/vf-error.h
+++ libraries/vf-common/public/vf-error.h
@@ -12,8 +12,13 @@ typedef enum {
     VF_FILE_ERR                    = 3,
 
     VF_CAMERA_INIT_ERR             = 4,
-    VF_CAMERA_CAPTURE_FRAME_FAILED = 5,
-    VF_CAMERA_DEINIT_ERR           = 6,
+    VF_CAMERA_START_ERR            = 5,
+    VF_CAMERA_STOP_ERR             = 6,
+    VF_CAMERA_CAPTURE_FRAME_FAILED = 7,
+    VF_CAMERA_DEINIT_ERR           = 8,
+
+    VF_CONV_INIT_ERR               = 9,
+    VF_CONV_PROCESSING_ERR         = 10,
 
     VF_ERROR_MAX
 } vf_err_t;
