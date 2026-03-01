01310862b86d4ed4e46d2bd093854a6914a6ab55
Radu Purecel <radu.purecel20@gmail.com>
2026-03-02 00:34:50 +0200

    libs: logger: Add logger API
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

--- libraries/vf-logger/public/vf-logger.h
+++ libraries/vf-logger/public/vf-logger.h
@@ -1,29 +1,88 @@
-#ifndef VF_LOGGER_H
-#define VF_LOGGER_H
+/**
+ **************************************************************************************************
+ *  @file           : vf-logger.h
+ *  @brief          : VF logger API Header
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  VisionFlow Logger implementation using Linux syslog and stdout
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
+
+#ifndef VF_LOGGER__H
+#define VF_LOGGER__H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <time.h>
+#include <string.h>
+
+#ifndef EOK
+#define EOK 0
+#endif
+
+#define VF_LOG_FATAL_STR   "FATAL"
+#define VF_LOG_ERR_STR     "ERROR"
+#define VF_LOG_WRN_STR     "WARNING"
+#define VF_LOG_INFO_STR    "INFO"
+#define VF_LOG_DBG_STR     "DEBUG"
+#define VF_LOG_TRACE_STR   "TRACE"
+#define VF_LOG_UNKNOWN_STR "UNKNOWN"
+
+/* Extract only the file name, without absolute path */
+#define VF_FILE_NAME_NO_PATH(filepath) \
+        (strrchr((filepath), '/') ? strrchr((filepath), '/') + 1 : (filepath))
+
+/* Base macro for logging dynamically by level */
+#define vf_log(log_level, fmt, ...)                                                                \
+        do {                                                                                       \
+                vf_logger_record(log_level,                                                        \
+                                 VF_FILE_NAME_NO_PATH(__FILE__),                                   \
+                                 __LINE__,                                                         \
+                                 __func__,                                                         \
+                                 fmt,                                                              \
+                                 ## __VA_ARGS__);                                                  \
+        } while (0)
+
+/* Fallback macro to print directly to standard (useful before logger is initialized) */
+#define vf_log_fprintf(fmt, ...)                                                                   \
+        do {                                                                                       \
+                fprintf(stderr,                                                                    \
+                        "[%s:%d:%s] [%s]: " fmt "\n",                                              \
+                        VF_FILE_NAME_NO_PATH(__FILE__),                                            \
+                        __LINE__,                                                                  \
+                        __func__,                                                                  \
+                        VF_LOG_ERR_STR,                                                            \
+                        ## __VA_ARGS__);                                                           \
+        } while (0)
 
 typedef enum {
-    LOG_INFO,
-    LOG_DEBUG,
-    LOG_WARNING,
-    LOG_ERROR,
-    LOG_FATAL
-} LogLevel;
+        VF_LOG_LEVEL_NONE  = 0,
+        VF_LOG_LEVEL_FATAL = 1,
+        VF_LOG_LEVEL_ERR   = 2,
+        VF_LOG_LEVEL_WRN   = 3,
+        VF_LOG_LEVEL_INFO  = 4,
+        VF_LOG_LEVEL_DBG   = 5,
+        VF_LOG_LEVEL_TRACE = 6
+} vf_log_level_t;
 
-void log_message(LogLevel level, const char *file, int line, const char *func, const char *fmt, ...);
-void set_log_level(LogLevel level);
-void set_log_file(const char *filename);
-void enable_console_log(int enabled);
+/* Main functions exposed by the API */
+int vf_logger_init(const char *app_name, vf_log_level_t max_verbosity);
+void vf_logger_deinit(void);
+void vf_logger_set_verbosity(vf_log_level_t log_level);
+void vf_logger_record(vf_log_level_t log_level, const char *file, int line, const char *func,
+                      const char *fmt, ...);
 
-#define log_info(fmt, ...)    log_message(LOG_INFO, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define log_warning(fmt, ...) log_message(LOG_WARNING, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define log_debug(fmt, ...)   log_message(LOG_DEBUG, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define log_error(fmt, ...)   log_message(LOG_ERROR, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#define log_fatal(fmt, ...)   log_message(LOG_FATAL, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+/* Macros for easy use in code */
+#define log_fatal(fmt, ...) vf_log(VF_LOG_LEVEL_FATAL, fmt, ##__VA_ARGS__)
+#define log_err(fmt, ...)   vf_log(VF_LOG_LEVEL_ERR,   fmt, ##__VA_ARGS__)
+#define log_wrn(fmt, ...)   vf_log(VF_LOG_LEVEL_WRN,   fmt, ##__VA_ARGS__)
+#define log_info(fmt, ...)  vf_log(VF_LOG_LEVEL_INFO,  fmt, ##__VA_ARGS__)
+#define log_dbg(fmt, ...)   vf_log(VF_LOG_LEVEL_DBG,   fmt, ##__VA_ARGS__)
+#define log_trace(fmt, ...) vf_log(VF_LOG_LEVEL_TRACE, fmt, ##__VA_ARGS__)
 
-#endif /* __VF_LOGGER_H__ */
+#endif /* VF_LOGGER__H */
 
--- libraries/vf-logger/src/vf-logger.c
+++ libraries/vf-logger/src/vf-logger.c
@@ -1,107 +1,282 @@
-#include <string.h>
+/**
+ **************************************************************************************************
+ *  @file           : vf-logger.c
+ *  @brief          : VF logger API
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  VisionFlow Logger implementation using Linux syslog and stdout
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
 
+#include <errno.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <time.h>
+
+#include "vf-error.h"
 #include "vf-logger.h"
 
-#define COLOR_RESET        "\033[0m"
-#define COLOR_INFO         "\033[94m"
-#define COLOR_WARNING      "\033[33m"
-#define COLOR_DEBUG        "\033[96m"
-#define COLOR_ERROR        "\033[31m"
-#define COLOR_FATAL        "\033[91m"
+#define DEFAULT_APP_NAME            "VisionFlow"
 
-#define VF_LOG_INFO_STR    "INFO"
-#define VF_LOG_WARNING_STR "WARNING"
-#define VF_LOG_DEBUG_STR   "DEBUG"
-#define VF_LOG_ERROR_STR   "ERROR"
-#define VF_LOG_FATAL_STR   "FATAL"
-#define VF_LOG_UNKNOWN_STR "UNKNOWN"
+#define VF_LOG_TIME_BUF_SIZE        64
+#define VF_LOG_SYSLOG_FMT_BUF_SIZE  512
 
-static LogLevel current_log_level = LOG_INFO;
-static const char *log_filename = "log.txt";
+/* ANSI colors for clean output in terminal */
+#define COLOR_RESET   "\x1b[0m"
+#define COLOR_FATAL   "\x1b[1;31m" /* Red Bold */
+#define COLOR_ERR     "\x1b[31m"   /* Red */
+#define COLOR_WRN     "\x1b[33m"   /* Yellow */
+#define COLOR_INFO    "\x1b[94m"   /* Green */
+#define COLOR_DBG     "\x1b[96m"   /* Cyan */
+#define COLOR_TRACE   "\x1b[37m"   /* White */
 
-static int console_logging_enabled = 1;
+typedef struct {
+        vf_log_level_t  max_level;
+        pthread_mutex_t lock;
+        bool            initialized;
+} vf_logger_ctx_t;
 
-void enable_console_log(int enabled) {
-    console_logging_enabled = enabled;
+/* Global and static logger context */
+static vf_logger_ctx_t g_logger = {
+        .max_level   = VF_LOG_LEVEL_TRACE,
+        .initialized = false
+};
+
+/* Map our logging level to the standard levels in syslog */
+static
+int map_to_syslog_level(vf_log_level_t level)
+{
+        switch (level) {
+            case VF_LOG_LEVEL_FATAL:
+                    return LOG_EMERG;
+            case VF_LOG_LEVEL_ERR:
+                    return LOG_ERR;
+            case VF_LOG_LEVEL_WRN:
+                    return LOG_WARNING;
+            case VF_LOG_LEVEL_INFO: 
+                    return LOG_INFO;
+            case VF_LOG_LEVEL_DBG:
+                    return LOG_DEBUG;
+            case VF_LOG_LEVEL_TRACE:
+                    return LOG_DEBUG;
+            default:
+                    return LOG_INFO;
+        }
 }
 
-void set_log_level(LogLevel level) {
-    current_log_level = level;
+/* Format the level text exactly as it was in the original */
+static
+const char *get_level_str_and_color(vf_log_level_t level, const char **color)
+{
+        if (NULL == color || NULL == *color) {
+                vf_log_fprintf("Invalid input: color = %p, *color = %p", color, *color);
+
+                return NULL;
+        }
+
+        switch (level) {
+                case VF_LOG_LEVEL_FATAL:
+                        *color = COLOR_FATAL;
+
+                        return VF_LOG_FATAL_STR;
+                case VF_LOG_LEVEL_ERR:
+                        *color = COLOR_ERR;
+
+                        return VF_LOG_ERR_STR;
+                case VF_LOG_LEVEL_WRN:
+                        *color = COLOR_WRN;
+
+                        return VF_LOG_WRN_STR;
+                case VF_LOG_LEVEL_INFO:
+                        *color = COLOR_INFO;
+
+                        return VF_LOG_INFO_STR;
+                case VF_LOG_LEVEL_DBG:
+                        *color = COLOR_DBG;
+
+                        return VF_LOG_DBG_STR;
+                case VF_LOG_LEVEL_TRACE:
+                        *color = COLOR_TRACE;
+
+                        return VF_LOG_TRACE_STR;
+                default:
+                        *color = COLOR_RESET;
+
+                        return VF_LOG_UNKNOWN_STR;
+        }
 }
 
-void set_log_file(const char *filename) {
-    log_filename = filename;
+int vf_logger_init(const char *app_name, vf_log_level_t max_verbosity)
+{
+        int err = EOK;
+
+        if (true == g_logger.initialized) {
+                return EOK; /* Already initialized */
+        }
+
+        err = pthread_mutex_init(&g_logger.lock, NULL);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to initialize logger mutex.");
+
+                return -1;
+        }
+
+        g_logger.max_level = max_verbosity;
+
+        if (NULL == app_name) {
+                app_name = DEFAULT_APP_NAME;
+        }
+
+        openlog(app_name, LOG_PID | LOG_CONS, LOG_USER);
+
+        g_logger.initialized = true;
+
+        return EOK;
 }
 
-static void get_timestamp(char *buffer, size_t size) {
-    time_t t = time(NULL);
-    struct tm *tm_info = localtime(&t);
-    strftime(buffer, size, "%Y-%m-%d %H:%M:%S", tm_info);
+void vf_logger_deinit(void)
+{
+        int err = EOK;
+
+        if (false == g_logger.initialized) {
+                return; /* Already deinitialized */
+        }
+
+        log_info("Logger shutting down...");
+
+        closelog();
+
+        err = pthread_mutex_destroy(&g_logger.lock);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to destroy logger mutex.");
+
+                return;
+        }
+
+        g_logger.initialized = false;
 }
 
-void log_message(LogLevel level, const char *file, int line, const char *func, const char *fmt, ...) {
-    if (level < current_log_level) {
-        return;
-    }
+void vf_logger_set_verbosity(vf_log_level_t log_level)
+{
+        int err = EOK;
 
-    const char *level_str;
-    const char *color;
+        err = pthread_mutex_lock(&g_logger.lock);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to lock logger mutex.");
 
-    switch (level) {
-        case LOG_INFO:
-            level_str = VF_LOG_INFO_STR;
-            color = COLOR_INFO;
-            break;
-        case LOG_WARNING:
-            level_str = VF_LOG_WARNING_STR;
-            color = COLOR_WARNING;
-            break;
-        case LOG_ERROR:
-            level_str = VF_LOG_ERROR_STR;
-            color = COLOR_ERROR;
-            break;
-        case LOG_DEBUG:
-            level_str = VF_LOG_DEBUG_STR;
-            color = COLOR_DEBUG;
-            break;
-        case LOG_FATAL:
-            level_str = VF_LOG_FATAL_STR;
-            color = COLOR_FATAL;
-            break;
-        default:
-            level_str = VF_LOG_UNKNOWN_STR;
-            color = COLOR_RESET;
-            break;
-    }
+                return;
+        }
 
-    const char *filename = strrchr(file, '/');
-    if (filename) {
-        filename++;
-    } else {
-        filename = file;
-    }
+        g_logger.max_level = log_level;
 
-    char timestamp[20];
-    get_timestamp(timestamp, sizeof(timestamp));
+        err = pthread_mutex_unlock(&g_logger.lock);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to unlock logger mutex.");
 
-    char message[512];
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(message, sizeof(message), fmt, args);
-    va_end(args);
+                return;
+        }
 
-    FILE *log_file = fopen(log_filename, "a");
-    if (log_file) {
-        fprintf(log_file, "[%s] [%s:%d:%s] [%s]: %s", timestamp, filename, line, func, level_str, message);
-        fclose(log_file);
-    }
+        log_info("Verbosity set successfully!");
+}
 
-    if (console_logging_enabled) {
-        printf("[%s] [%s:%d:%s] %s[%s]%s: %s", timestamp, filename, line, func, color, level_str, COLOR_RESET, message);
-    }
+void vf_logger_record(vf_log_level_t log_level, const char *file, int line, const char *func,
+        const char *fmt, ...)
+{
+        char time_buf[VF_LOG_TIME_BUF_SIZE] = {0};
+        char syslog_fmt[VF_LOG_SYSLOG_FMT_BUF_SIZE]= {0};
+        const char *color = NULL;
+        const char *level_str = NULL;
+        va_list args_console = {0};
+        va_list args_syslog = {0};
+        struct timespec ts = {0};
+        struct tm tm_info = {0};
+        int syslog_level = 0;
+        long millis = 0;
+        int err = EOK;
 
-    if (level == LOG_FATAL) {
-        exit(EXIT_FAILURE);
-    }
+        if (NULL == file || NULL == func || NULL == fmt) {
+                vf_log_fprintf("Invalid input: file = %p, func = %p, fmt = %p",
+                               (void *)file, (void *)func, (void *)fmt);
+
+                return;
+        }
+
+        if (log_level < VF_LOG_LEVEL_FATAL || log_level > VF_LOG_LEVEL_TRACE) {
+                vf_log_fprintf("Invalid log level provided: %d. Message dropped.",
+                               log_level);
+
+                return;
+        }
+
+        if (false == g_logger.initialized || log_level > g_logger.max_level) {
+                return;
+        }
+
+        /* 1. Get the current time function thread-safe localtime_r */
+        err = clock_gettime(CLOCK_REALTIME, &ts);
+        if (EOK != err) {
+                log_err("Failed to get current time", strerror(errno));
+
+                return;
+        }
+
+        (void)localtime_r(&ts.tv_sec, &tm_info);
+
+        /* Calculate miliseconds from nanoseconds */
+        millis = ts.tv_nsec / 1000000;
+
+        (void)strftime(time_buf, VF_LOG_TIME_BUF_SIZE, "%Y-%m-%d %H:%M:%S", &tm_info);
+
+        /* 2. Prepare the variable arguments */
+        va_start(args_console, fmt);
+        va_copy(args_syslog, args_console);
+
+        color = COLOR_RESET;
+        level_str = get_level_str_and_color(log_level, &color);
+        if (NULL == level_str || NULL == color) {
+                vf_log_fprintf("Invalid internal color/level pointers. color = %p, level = %p",
+                               (void *)color, (void *)level_str);
+
+                goto cleanup;
+        }
+
+        err = pthread_mutex_lock(&g_logger.lock);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to lock logger mutex.");
+
+                goto cleanup;
+        }
+
+        /* 3. Display in the console using your exact original format */
+        (void)fprintf(stderr, "[%s.%.3ld] [%s:%d:%s] %s[%s]%s: ",
+                      time_buf, millis, file, line, func, color, level_str, COLOR_RESET);
+        (void)vfprintf(stderr, fmt, args_console);
+        (void)fprintf(stderr, "\n");
+
+        /* 4. Send to syslog (without colors and without explicit time, syslog adds its time) */
+        (void)snprintf(syslog_fmt, sizeof(syslog_fmt), "[%s:%d:%s] [%s]: %s", file, line, func,
+                       level_str, fmt);
+
+        syslog_level = map_to_syslog_level(log_level);
+
+        vsyslog(syslog_level, syslog_fmt, args_syslog);
+
+        err = pthread_mutex_unlock(&g_logger.lock);
+        if (err != EOK) {
+                vf_log_fprintf("Failed to unlock logger mutex.");
+        }
+
+cleanup:
+        va_end(args_console);
+        va_end(args_syslog);
 }
 
