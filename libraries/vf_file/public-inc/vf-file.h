1838c7c4d8e9fa35379b061305e43ccdc72f53d7
Radu Purecel <radu.purecel20@gmail.com>
2026-04-14 12:41:28 +0300

    libs: file: Add file API
    [VF-19]
    
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

---
+++ libraries/vf_file/public-inc/vf-file.h
@@ -0,0 +1,45 @@
+/**
+ **************************************************************************************************
+ *  @file           : vf-file.h
+ *  @brief          : VisionFlow File I/O API Header
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  VisionFlow thin wrapper over standard file I/O operations with consistent
+ *  vf_err_t error handling
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
+
+#ifndef VF_FILE_H
+#define VF_FILE_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "vf-error.h"
+
+typedef struct {
+        FILE   *fp;
+        char    path[256];
+        int     is_open;
+} vf_file_t;
+
+vf_err_t vf_file_open(vf_file_t *file, const char *path, const char *mode);
+vf_err_t vf_file_close(vf_file_t *file);
+
+vf_err_t vf_file_read(vf_file_t *file, void *buf, size_t size, size_t *bytes_read);
+vf_err_t vf_file_write(vf_file_t *file, const void *buf, size_t size, size_t *bytes_written);
+
+vf_err_t vf_file_size(vf_file_t *file, size_t *out_size);
+vf_err_t vf_file_exists(const char *path, int *out_exists);
+vf_err_t vf_file_seek(vf_file_t *file, long offset, int whence);
+vf_err_t vf_file_tell(vf_file_t *file, long *out_pos);
+
+#endif /* VF_FILE_H */
+
