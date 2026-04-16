c5b330fbc6ef98830d983e289a134320988cc0f3
Radu Purecel <radu.purecel20@gmail.com>
2026-04-16 13:55:33 +0300

    service: buffer_mgr: Add implementation of buffer mgr
    [VF-23]
    
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

---
+++ service/public-inc/vf-buff-pool.h
@@ -0,0 +1,61 @@
+/**
+ **************************************************************************************************
+ *  @file           : vf-buf-pool.h
+ *  @brief          : VisionFlow Buffer Pool API Header
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  Fixed-size pool of pre-allocated vf_framebuffer_t slots. All framebuffers are
+ *  allocated at init time. No dynamic allocation occurs at runtime. Thread-safe.
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
+
+#ifndef VF_BUFF_POOL_H
+#define VF_BUFF_POOL_H
+
+#include <pthread.h>
+#include <stdint.h>
+
+#include "vf-error.h"
+#include "vf-framebuffer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VF_BUF_POOL_MAX_SLOTS 16U
+
+typedef struct {
+        vf_framebuffer_t  slots[VF_BUF_POOL_MAX_SLOTS];
+        uint8_t           in_use[VF_BUF_POOL_MAX_SLOTS];
+        uint32_t          capacity;
+        uint32_t          available;
+        pthread_mutex_t   lock;
+        int               initialized;
+} vf_buf_pool_t;
+
+vf_err_t vf_buf_pool_init(vf_buf_pool_t        *pool,
+                           const vf_fb_params_t *params,
+                           uint32_t              slot_count);
+
+vf_err_t vf_buf_pool_acquire(vf_buf_pool_t     *pool,
+                              vf_framebuffer_t **out_fb);
+
+vf_err_t vf_buf_pool_release(vf_buf_pool_t    *pool,
+                              vf_framebuffer_t *fb);
+
+void     vf_buf_pool_deinit(vf_buf_pool_t *pool);
+
+uint32_t vf_buf_pool_available(vf_buf_pool_t *pool);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VF_BUFF_POOL_H */
+
---
+++ service/public-inc/vf-buff-queue.h
@@ -0,0 +1,62 @@
+/**
+ **************************************************************************************************
+ *  @file           : vf-buf-queue.h
+ *  @brief          : VisionFlow Buffer Queue API Header
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  Fixed-capacity circular FIFO queue of vf_framebuffer_t pointers for inter-unit
+ *  frame passing. No dynamic allocation. Thread-safe.
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
+
+#ifndef VF_BUFF_QUEUE_H
+#define VF_BUFF_QUEUE_H
+
+#include <pthread.h>
+#include <stdint.h>
+
+#include "vf-error.h"
+#include "vf-framebuffer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VF_BUF_QUEUE_MAX_CAPACITY 16U
+
+typedef struct {
+        vf_framebuffer_t *slots[VF_BUF_QUEUE_MAX_CAPACITY];
+        uint32_t          head;
+        uint32_t          tail;
+        uint32_t          count;
+        uint32_t          capacity;
+        pthread_mutex_t   lock;
+        int               initialized;
+} vf_buf_queue_t;
+
+vf_err_t vf_buf_queue_init(vf_buf_queue_t *queue, uint32_t capacity);
+
+vf_err_t vf_buf_queue_push(vf_buf_queue_t   *queue,
+                            vf_framebuffer_t *fb);
+
+vf_err_t vf_buf_queue_pop(vf_buf_queue_t    *queue,
+                           vf_framebuffer_t **out_fb);
+
+void     vf_buf_queue_deinit(vf_buf_queue_t *queue);
+
+uint32_t vf_buf_queue_count(vf_buf_queue_t *queue);
+int      vf_buf_queue_is_full(vf_buf_queue_t *queue);
+int      vf_buf_queue_is_empty(vf_buf_queue_t *queue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VF_BUFF_QUEUE_H */
+
