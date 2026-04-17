bd45fadb46b5e5606021d127e186c8d4696a4f6f
Radu Purecel <radu.purecel20@gmail.com>
2026-04-17 10:17:05 +0300

    service: core: Add core API
    [VF-29]
    
    Signed-off-by: Radu Purecel <radu.purecel20@gmail.com>

---
+++ service/public-inc/vf-core.h
@@ -0,0 +1,33 @@
+/**
+ **************************************************************************************************
+ *  @file           : vf-core.h
+ *  @brief          : VisionFlow Pipeline Core Header
+ **************************************************************************************************
+ *  @author     Radu Purecel
+ *
+ *  @description:
+ *  Entry point for the VisionFlow pipeline execution.
+ *
+ *  @section  HISTORY
+ *  v1.0  - First version
+ *
+ **************************************************************************************************
+ */
+
+#ifndef VF_CORE_H
+#define VF_CORE_H
+
+#include "vf-error.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+vf_err_t vf_core_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VF_CORE_H */
+
